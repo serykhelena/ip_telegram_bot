345c6092dc2004a7512fc3af9455ef05a46899de
NUCuser <NUCuser@sau.ru>
2019-09-13 19:43:37 +0300

    Logging was added, polling 1 Hz

--- bot.py
+++ bot.py
@@ -1,36 +1,59 @@
+#!/usr/bin/env python
 import telebot
 import socket
+import logging
+from pyspectator.processor import Cpu 
+from psutil import sensors_temperatures
+
 
 with open('bot_token.txt', 'r') as vip_file:
     TOKEN = vip_file.read()
 
-UNICORN_STICKER = "CAADAgAD5wIAAkcVaAnnQcbyzCQWrhYE"
-NOT_WORKING_STICKER = "AAQCAAMNAwAChmBXDmIhZ5pao3rFmOCaDwAEAQAHbQADHVkAAhYE"
+UNICORN_STICKER = "CAADAgADKQwAAiMhBQAB4UzwzHmfF30WBA"
 
 if TOKEN == 0:
     print("No token at all")
 
 bot = telebot.TeleBot(TOKEN)
+# logger = telebot.logger
 
-print("Ok")
+cpu = Cpu(monitoring_latency=1)
 
-@bot.message_handler(commands=['ip'])
+def getCPUtemperature():
+    # temp Core 1 current 
+    return sensors_temperatures()["coretemp"][1][1]
+    
+def getCPUload():
+    return str(cpu.load)
+
+@bot.message_handler(commands=['info'])
 def command_start(message):
     hostname = socket.gethostname()
-
+    # AF_INET for using IPv4 
+    # SOCK_STREAM for TCP
+    # SOCK_DGRAM for UDP
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
-
+    # s.getsockname() return the socket's own address
+    # zB. ('127.0.0.1', 10000)
     bot.reply_to(message,
                  'Hello, I\'m a demo IP Teller bot!\n'
                  'NUC Machine Name is: ' + hostname + '\n'
-                 'NUC IP Address is: ' + s.getsockname()[0]
+                 'NUC IP Address is: ' + s.getsockname()[0] + '\n'
+                 'CPU temperature is: ' + str(getCPUtemperature()) + '\n'
+                 'CPU load is: ' + getCPUload() + '\n'
                  )
     bot.send_sticker(message.chat.id, UNICORN_STICKER)
 
+# just for fun 
 @bot.message_handler(content_types=['sticker'])
 def sticker_handler(message):
     print(message.sticker)
 
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    try:
+        bot.polling(none_stop=True, interval=1)
+    except Exception as e:
+        logging.basicConfig(filename='ip_bot_log.log', filemode='a', 
+                            format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.exception(e)
